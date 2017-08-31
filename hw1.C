dea9f15a1ad8801b280e7deaf39d8134d559fb80
eyandel <eyandel16@gmail.com>
2017-08-30 22:36:05 -0400

    added nrvector

--- hw1.C
+++ hw1.C
@@ -1,13 +1,15 @@
 #include <vector>
+#include "nr3.h"
 using namespace std;
 
 void hw1(){
-  vector<double> x(2);
-  x.at(0) = 1;
-  x.at(1) = -1;
+  cout<< setprecision(8);
+  VecDoub x(2);
+  x[0] = 1;
+  x[1] = -1;
   std::cout<<x[0]<<std::endl;
-  vector<double> b(2);
-  b.at(0) = 0.1869169;
-  b.at(1) = 0.2155734;
+  VecDoub b(2);
+  b[0] = 0.1869169;
+  b[1] = 0.2155734;
   std::cout<<b[1]<<std::endl;
 }
